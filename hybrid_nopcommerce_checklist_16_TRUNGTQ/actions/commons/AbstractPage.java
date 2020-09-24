1b6f20800b4f194a28cfad388b0ae217918d763e
TrungTran205 <tranquoctrung90@gmail.com>
2020-09-24 23:41:18 +0700

    AbstractPage (58:00-end)
    Web Browser
    Web Element (1- 17)

--- hybrid_nopcommerce_checklist_16_TRUNGTQ/actions/commons/AbstractPage.java
+++ hybrid_nopcommerce_checklist_16_TRUNGTQ/actions/commons/AbstractPage.java
@@ -1,9 +1,228 @@
-/**
- * 
- */
-/**
- * @author Trung-PC
- * Chứa những hàng dùng chung cho tầng Page Object
- *
- */
 package commons;
+
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AbstractPage {
+	int waitingTime = 30;
+
+	// Browser
+	protected void openPageUrl(WebDriver driver, String url) {
+		driver.get(url);
+	}
+
+	protected String getCurrentPageUrl(WebDriver driver) {
+		return driver.getCurrentUrl();
+	}
+
+	protected String getCurrentPageTitle(WebDriver driver) {
+		return driver.getTitle();
+	}
+
+	protected String getCurrentPageSource(WebDriver driver) {
+		return driver.getPageSource();
+	}
+
+	protected void backToPage(WebDriver driver) {
+		driver.navigate().back();
+	}
+
+	protected void forwardToPage(WebDriver driver) {
+		driver.navigate().forward();
+	}
+
+	protected void refreshCurrentPage(WebDriver driver) {
+		driver.navigate().refresh();
+	}
+
+	protected void switchToAlert(WebDriver driver) {
+		driver.switchTo().alert();
+	}
+
+	protected void acceptAlert(WebDriver driver) {
+		driver.switchTo().alert().accept();
+	}
+
+	protected void dismissAlert(WebDriver driver) {
+		driver.switchTo().alert().dismiss();
+	}
+
+	protected String getTextAlert(WebDriver driver) {
+		return driver.switchTo().alert().getText();
+	}
+
+	protected void sendkeysToAlert(WebDriver driver, String keyword) {
+		driver.switchTo().alert().sendKeys(keyword);
+	}
+
+	protected void sendCredentialToAlert(WebDriver driver, String credential) {
+		driver.switchTo().alert().sendKeys(credential);
+	}
+
+	protected void waitAlertPresence(WebDriver driver) {
+		WebDriverWait explicitWait = new WebDriverWait(driver, waitingTime);
+		explicitWait.until(ExpectedConditions.alertIsPresent());
+	}
+
+	protected void switchToWindowByID(WebDriver driver, String parentID) {
+		Set<String> allWindows = driver.getWindowHandles();
+		for (String runWindow : allWindows) {
+			if (!runWindow.equals(parentID)) {
+				driver.switchTo().window(runWindow);
+				break;
+			}
+
+		}
+	}
+
+	protected void switchToWindowByTitle(WebDriver driver, String title) {
+		Set<String> allWindows = driver.getWindowHandles();
+		for (String runWindow : allWindows) {
+			driver.switchTo().window(runWindow);
+			String currentPageTitle = driver.getTitle();
+			if (currentPageTitle.equals(title))
+				break;
+		}
+	}
+
+	protected void closeAllWindowWithourParent(WebDriver driver, String parentID) {
+		Set<String> allWindows = driver.getWindowHandles();
+		for (String runWindow : allWindows) {
+			if (!runWindow.equals(parentID)) {
+				driver.switchTo().window(runWindow);
+				driver.close();
+			}
+		}
+		driver.switchTo().window(parentID);
+	}
+
+	private By getByXpath(String locator) {
+		return By.xpath(locator);
+	}
+
+	private WebElement getElement(WebDriver driver, String locator) {
+		return driver.findElement(getByXpath(locator));
+	}
+
+	private List<WebElement> getElements(WebDriver driver, String locator) {
+		return driver.findElements(By.xpath(locator));
+	}
+
+	protected void clickToElement(WebDriver driver, String locator) {
+		WebElement element = getElement(driver, locator);
+		element.click();
+
+	}
+
+	protected void sendkeyToElement(WebDriver driver, String locator, String value) {
+		WebElement element = getElement(driver, locator);
+		element.clear();
+		element.sendKeys(value);
+	}
+
+	protected void selectItemInDropdown(WebDriver driver, String locator, String itemValue) {
+		WebElement element = getElement(driver, locator);
+		Select select = new Select(element);
+		select.selectByVisibleText(itemValue);
+	}
+
+	protected String getFirstSelectedTextInDropDown(WebDriver driver, String locator) {
+		WebElement element = getElement(driver, locator);
+		Select select = new Select(element);
+		return select.getFirstSelectedOption().getText();
+	}
+
+	protected boolean isDropdownMultiple(WebDriver driver, String locator) {
+		WebElement element = getElement(driver, locator);
+		Select select = new Select(element);
+		return select.isMultiple();
+	}
+
+	protected void selectItemInCustomDropdown(WebDriver driver, String parentLocator, String childItemLocator,
+			String expectedItem) {
+		// 1. Click vao the cha de xo ra tat ca item con
+		getElement(driver, parentLocator).click();
+		// 2. Cho cho tat ca item duoc load ra
+		WebDriverWait explicitedWait = new WebDriverWait(driver, waitingTime);
+		explicitedWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(getByXpath(childItemLocator)));
+		// 3. Dua tat ca cac item vao 01 list de kiem tra
+		List<WebElement> allItems = getElements(driver, childItemLocator);
+		// 4. Chay qua tat ca cac gia tri dang co trong list
+		for (WebElement item : allItems) {
+			// 5. Kiem tra xem text cua gia co co item nao bang item mong muon khong
+			if (item.getText().equals(expectedItem)) {
+				// Roll xuong den dung item nay
+				JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+				jsExecutor.executeScript("arguments[0].scrollIntoView(true);", item);
+				item.click();
+				break;
+			}
+		}
+	}
+
+	private void sleepInSecond(long timeout) {
+		try {
+			Thread.sleep(timeout * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	protected String getElementAttribute(WebDriver driver, String locator, String attributeName) {
+		WebElement element = getElement(driver, locator);
+		return element.getAttribute(locator);
+	}
+
+	protected String getElementText(WebDriver driver, String locator) {
+		WebElement element = getElement(driver, locator);
+		return element.getText();
+	}
+
+	protected int countElementSize(WebDriver driver, String locator) {
+		return getElements(driver, locator).size();
+	}
+
+	protected void checkToCheckbox(WebDriver driver, String locator) {
+		WebElement element = getElement(driver, locator);
+		if (!element.isSelected()) {
+			element.click();
+		}
+	}
+
+	protected void uncheckToCheckbox(WebDriver driver, String locator) {
+		WebElement element = getElement(driver, locator);
+		if (element.isSelected()) {
+			element.click();
+		}
+	}
+
+	protected boolean isElementDisplayed(WebDriver driver, String locator) {
+		return getElement(driver, locator).isDisplayed();
+	}
+
+	protected boolean isElementEnabled(WebDriver driver, String locator) {
+		return getElement(driver, locator).isEnabled();
+	}
+
+	protected boolean isElementSelected(WebDriver driver, String locator) {
+		return getElement(driver, locator).isSelected();
+	}
+
+	protected void switchToFrame(WebDriver driver, String locator) {
+		driver.switchTo().frame(getElement(driver, locator));
+	}
+
+	protected void switchToDefaultContent(WebDriver driver) {
+		driver.switchTo().defaultContent();
+	}
+
+}
--- hybrid_nopcommerce_checklist_16_TRUNGTQ/actions/commons/AbstractTest.java
+++ hybrid_nopcommerce_checklist_16_TRUNGTQ/actions/commons/AbstractTest.java
@@ -1,9 +1,11 @@
-/**
- * 
- */
-/**
- * @author Trung-PC
- * Chứa những hàng dùng chung cho tầng Test Case
- *
- */
+
 package commons;
+
+import java.util.Random;
+
+public class AbstractTest {
+	protected int randomNumber () {
+		Random randomNumber = new Random();
+		return randomNumber.nextInt(99999999);
+	}
+}
